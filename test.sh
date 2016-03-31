1caa92af517e8b09db2f72f14c15237d32b9dc62
Gabriel Schenker <gnschenker@gmail.com>
2016-03-31 16:47:39 -0500

    Polished test script

--- test.sh
+++ test.sh
@@ -1,15 +1,9 @@
-echo "List of environment variables"
-env
 echo "========================="
 echo "Now sleeping for 10"
-sleep 10
+sleep 10   #give the web api time to start up...
 echo "========================="
-#url="http://192.168.99.100/api/projects"
 url="web:5000/api/projects"
 echo "now accessing: ${url}"
-echo "Now using the 'dig web' command:"
-dig web
-echo "========================="
 if curl ${url} | grep -q '{"id":1,"name":"Heart Beat"}'; then
   echo "Test passed!"
   exit 0
