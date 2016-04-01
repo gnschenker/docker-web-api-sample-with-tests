2e1cc1df7d237cf57fce8f24533a774ab377de72
Gabriel N. Schenker <gnschenker@gmail.com>
2016-04-01 10:00:57 -0500

    Add failing test

--- test.sh
+++ test.sh
@@ -1,10 +1,18 @@
 echo "Waiting for Web API to be ready"
 sleep 12
-if curl web:5000/api/projects | grep -q '{"id":1,"name":"Heart Beat"}'; then
-  echo "Success"
-  exit 0
+
+if curl web:5000/api/projects/5 | grep -q '{"id":5,"name":"Mystery"}'; then
+  echo "api/projects/5 succeeded"
 else
-  echo "Fail!"
+  echo "api/projects/5 FAILED"
   exit 1
 fi
+
+if curl web:5000/api/projects | grep -q '{"id":1,"name":"Heart Beat"}'; then
+  echo "api/projects succeeded"
+else
+  echo "api/projects FAILED"
+  exit 1
+fi
+exit 0
 
